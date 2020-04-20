1bc8129ff95e025abaeb92a1aa25e17ea3ef5211
George Kouklakis <georgekouklakis@gmail.com>
2020-04-21 01:46:47 +0200

    chore: add eslint eol rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -28,6 +28,10 @@ module.exports = {
         'semi': [
             'error',
             'always'
+        ],
+        'eol-last': [
+            'error',
+            'always'
         ]
     }
 };
